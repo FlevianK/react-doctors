31f0e33fb1c34234451d99a01e902627c6778d7c
FlevianK <flevian.kanaiza@andela.com>
2019-09-04 21:12:48 +0300

    Refactored doctordetails file by introducing react hooks

--- src/components/doctor/DoctorsDetails.js
+++ src/components/doctor/DoctorsDetails.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
@@ -26,88 +26,77 @@ const LoadingTextWrapper = styled.div`
   padding-top: 0.3rem;
 `;
 
-export class DoctorsDetails extends Component {
-  // eslint-disable-next-line react/sort-comp
-  Initialstate = {
-    limit: 15,
-    offset: 0,
-    radius: 100,
-    activePage: 1,
-    hasNextPage: false
+const DoctorsDetails = (props) => {
+  const { getDoctors, doctorsDetails } = props;
+  const [limit, setLimit] = useState(15);
+  const [offset, setOffest] = useState(0);
+  const [radius, setRadius] = useState(100);
+  const [activePage, setActivePage] = useState(1);
+  const [hasNextPage, setHasNextPage] = useState(false);
+  const [searchValue, setSearchValue] = useState('my-current-location');
+
+  useEffect(() => {
+    getDoctors(searchValue, limit, offset, radius);
+  });
+
+  useEffect(() => {
+    if (offset + limit < doctorsDetails.total) {
+      setHasNextPage(true);
+    } else {
+      setHasNextPage(false);
+    }
+  }, [doctorsDetails, limit, offset]);
+
+  const handlePageChange = value => () => {
+    getDoctors(searchValue, limit, offset, radius);
+    if (value) {
+      setOffest(offset + limit);
+      setActivePage(activePage + 1);
+    } else {
+      setOffest(offset - limit);
+      setActivePage(activePage - 1);
+    }
   };
 
-  state = {
-    searchValue: 'my-current-location',
-    limit: 15,
-    offset: 0,
-    radius: 100,
-    activePage: 1,
-    doctorsDetails: {},
-    hasNextPage: false
-  };
-
-  componentDidMount () {
-    const { searchValue, limit, offset, radius } = this.state;
-    this.props.getDoctors(searchValue, limit, offset, radius);
-  }
-
-  static getDerivedStateFromProps (nextProps, prevState) {
-    return prevState.offset + prevState.limit < nextProps.doctorsDetails.total ?
-      { doctorsDetails: nextProps.doctorsDetails, hasNextPage: true } :
-      { doctorsDetails: nextProps.doctorsDetails, hasNextPage: false };
-  }
-
-  handlePageChange = value => () => {
-    let { offset, limit, radius, searchValue, activePage } = this.state;
-    this.props.getDoctors(searchValue, limit, offset, radius);
-    return value ?
-      this.setState({ offset: offset + limit, activePage: ++activePage }) :
-      this.setState({ offset: offset - limit, activePage: --activePage });
-  };
-
-  onDoctorChange = event => this.setState({ searchValue: event.target.value });
-
-  onDoctorClick = event => {
+  const onDoctorClick = event => {
     event.preventDefault();
-    const { searchValue } = this.state;
-    const { offset, limit, radius } = this.Initialstate;
-    return searchValue ?
-      (this.setState(prevState => ({ ...prevState, ...this.Initialstate })),
-      this.props.getDoctors(searchValue, limit, offset, radius)) :
+    if (searchValue) {
+      setOffest(0);
+      setLimit(15);
+      setActivePage(1);
+      setRadius(100);
+      setHasNextPage(false);
+      getDoctors(searchValue, limit, offset, radius);
+      setSearchValue('my-current-location');
+    } else {
       toastr.error('Type location name');
+    }
   };
 
-  onKeyPress = event => {
-    return event.key === 'Enter' && this.onDoctorClick(event);
-  };
-
-  render () {
-    const { doctorsDetails, hasNextPage } = this.state;
-    return (
-      <DoctorsDetailsWrapper>
-        <DoctorsSearch
-          onDoctorChange={this.onDoctorChange}
-          onDoctorClick={this.onDoctorClick}
-          onKeyPress={this.onKeyPress}
+  return (
+    <DoctorsDetailsWrapper>
+      <DoctorsSearch
+        onDoctorChange={e => setSearchValue(e.target.value)}
+        onDoctorClick={onDoctorClick}
+        onKeyPress={e => e.key === 'Enter' && onDoctorClick(e)}
+      />
+      {doctorsDetails.data && doctorsDetails.data.length ? (
+        <DoctoctsList
+          doctorsDetails={doctorsDetails}
+          handlePageChange={handlePageChange}
+          activePage={activePage}
+          hasNextPage={hasNextPage}
         />
-        {doctorsDetails.data && doctorsDetails.data.length ? (
-          <DoctoctsList
-            doctorsDetails={doctorsDetails}
-            handlePageChange={this.handlePageChange}
-            activePage={this.state.activePage}
-            hasNextPage={hasNextPage}
-          />
-        ) : (
-          <LoadingGifWrapper>
-            <LoadingGif />
-            <LoadingTextWrapper>
+      ) : (
+        <LoadingGifWrapper>
+          <LoadingGif />
+          <LoadingTextWrapper>
               Loading Current location data
-            </LoadingTextWrapper>
-          </LoadingGifWrapper>
-        )}
-      </DoctorsDetailsWrapper>
-    );
-  }
+          </LoadingTextWrapper>
+        </LoadingGifWrapper>
+      )}
+    </DoctorsDetailsWrapper>
+  );
 }
 DoctorsDetails.propTypes = {
   doctorsDetails: PropTypes.object,
